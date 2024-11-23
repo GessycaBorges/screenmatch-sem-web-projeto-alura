02828f05f5bdb180fdba05c60f48be44c95bf445
Gessyca Borges <gessyca.borges@gmail.com>
2024-11-22 22:38:17 -0300

    Manipulando com fluxos

---
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -0,0 +1,78 @@
+package br.com.alura.screenmatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer episodio;
+    private double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.episodio = dadosEpisodio.episodio();
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException e) {
+            this.avaliacao = 0.0;
+        }
+        try {
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeParseException e){
+            this.dataLancamento = null;
+        }
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getEpisodio() {
+        return episodio;
+    }
+
+    public void setEpisodio(Integer episodio) {
+        this.episodio = episodio;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "Temporada= " + temporada +
+                ", titulo= '" + titulo + '\'' +
+                ", episodio= " + episodio +
+                ", avaliacao= " + avaliacao +
+                ", dataLancamento= " + dataLancamento +
+                '}';
+    }
+}
